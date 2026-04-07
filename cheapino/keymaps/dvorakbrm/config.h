e2348e1c4614d5552f37af50133e278b5df6524c
Chris O'Callaghan <christopher.l.ocallaghan@gmail.com>
2026-04-07 16:11:26 -0400

    added config.h and rules.mk files

---
+++ cheapino/keymaps/dvorakbrm/config.h
@@ -0,0 +1,10 @@
+#define COMBO_COUNT 2
+#define RAW_EPSIZE 32
+#define PAYLOAD_MARK 0x90
+#define PAYLOAD_BEGIN 24
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
+
+// Not required for Vial
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE 0x61
